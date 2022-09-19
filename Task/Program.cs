d052b1cc6b260e884a1b1a78892c5afbe97d1cb2
NikitaRazuvaev <90686956+NikitaRazuvaev@users.noreply.github.com>
2022-09-19 13:19:34 +0300

    Writing a program for the final test work

---
+++ Task/Program.cs
@@ -0,0 +1,28 @@
+﻿void SecondArrayWithIF(string[] arraysecond, string[] arraytwo)
+{
+    int count = 0;
+    for (int i = 0; i < arraysecond.Length; i++)
+    {
+        if (arraysecond[i].Length <= 3)
+        {
+            arraytwo[count] = arraytwo[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.Clear();
+
+string[] arraysecond = new string[4] { "hello", "2", "world", ":-)" };
+string[] arraytwo = new string[arraysecond.Length];
+
+SecondArrayWithIF(arraysecond, arraytwo);
+PrintArray(arraytwo);
